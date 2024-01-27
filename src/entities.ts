319814943a9404a3c0df3a29ee473f0483ad3858
Nassim <jslegend@protonmail.com>
2024-01-27 12:39:13 -0500

    got the inhaling mechanic working still WIP

--- src/entities.ts
+++ src/entities.ts
@@ -16,11 +16,11 @@ type PlayerGameObj = GameObj<
     BodyComp &
     PosComp &
     ScaleComp &
-    DoubleJumpComp & { speed: number }
+    DoubleJumpComp & { speed: number; direction: string; isInhaling: boolean }
 >;
 
 export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
-  return k.make([
+  const player = k.make([
     k.sprite("assets", { anim: "kirbIdle" }),
     k.area({ shape: new k.Rect(k.vec2(4, 5.9), 8, 10) }),
     k.body(),
@@ -29,23 +29,44 @@ export function makePlayer(k: KaboomCtx, posX: number, posY: number) {
     k.doubleJump(10),
     {
       speed: 300,
+      direction: "right",
+      isInhaling: false,
     },
-    "kirb",
+    "player",
   ]);
+
+  const inhaleZone = player.add([
+    k.area({ shape: new k.Rect(k.vec2(0), 16, 8) }),
+    k.pos(14, 5),
+    "inhaleZone",
+  ]);
+
+  inhaleZone.onUpdate(() => {
+    if (player.direction === "left") {
+      inhaleZone.pos = k.vec2(-14, 5);
+      return;
+    }
+    inhaleZone.pos = k.vec2(14, 5);
+  });
+
+  return player;
 }
 
 export function setControls(k: KaboomCtx, player: PlayerGameObj) {
   k.onKeyDown((key) => {
     switch (key) {
       case "left":
+        player.direction = "left";
         player.flipX = true;
         player.move(-player.speed, 0);
         break;
       case "right":
+        player.direction = "right";
         player.flipX = false;
         player.move(player.speed, 0);
         break;
       case "z":
+        player.isInhaling = true;
         player.play("kirbInhaling");
         break;
       default:
@@ -62,6 +83,7 @@ export function setControls(k: KaboomCtx, player: PlayerGameObj) {
   k.onKeyRelease((key) => {
     switch (key) {
       case "z":
+        player.isInhaling = false;
         player.play("kirbIdle");
         break;
       default:
@@ -81,14 +103,42 @@ export function makeFlameEnemy(k: KaboomCtx, posX: number, posY: number) {
 }
 
 export function makeGuyEnemy(k: KaboomCtx, posX: number, posY: number) {
-  k.add([
+  const guy = k.add([
     k.sprite("assets", { anim: "guyWalk" }),
     k.scale(scale),
     k.pos(posX * scale, posY * scale),
     k.area({ shape: new k.Rect(k.vec2(2, 3.9), 12, 12) }),
     k.body(),
+    { isInhalable: false },
     "guy",
   ]);
+
+  guy.onCollide("inhaleZone", () => {
+    guy.isInhalable = true;
+  });
+
+  guy.onCollideEnd("inhaleZone", () => {
+    guy.isInhalable = false;
+  });
+
+  const playerRef = k.get("player")[0];
+  guy.onUpdate(() => {
+    if (playerRef.isInhaling && guy.isInhalable) {
+      if (playerRef.direction === "right") {
+        guy.moveTo(guy.pos.sub(playerRef.pos), 100);
+        return;
+      }
+      guy.moveTo(guy.pos.add(playerRef.pos), 100);
+    }
+  });
+
+  guy.onCollide("player", () => {
+    if (!playerRef.isInhaling) return;
+    playerRef.isInhaling = false;
+    k.destroy(guy);
+  });
+
+  return guy;
 }
 
 export function makeBirdEnemy(k: KaboomCtx, posX: number, posY: number) {
