28b658d5fc5ce5b50040ba599953c31978419bae
Nassim <jslegend@protonmail.com>
2024-02-01 10:08:54 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,6 @@ import {
 } from "./entities";
 import { globalGameState } from "./state";
 
-k.loadSprite("assets", "./kirby-like.png", {
-  sliceX: 9,
-  sliceY: 10,
-  anims: {
-    kirbIdle: 0,
-    kirbInhaling: 1,
-    kirbFull: 2,
-    kirbInhaleEffect: { from: 3, to: 8, speed: 15, loop: true },
-    shootingStar: 9,
-    flame: { from: 36, to: 37, speed: 4, loop: true },
-    guyIdle: 18,
-    guyWalk: { from: 18, to: 19, speed: 4, loop: true },
-    bird: { from: 27, to: 28, speed: 4, loop: true },
-  },
-});
-k.loadSprite("level-1", "./level-1.png");
-
 async function gameSetup() {
   k.loadSprite("assets", "./kirby-like.png", {
     sliceX: 9,
